462477ae2ea2f039d9b936433f96bbc97eb7147b
Taylor Price <tayworm@gmail.com>
2024-05-06 09:49:49 -0700

    chore: a few tweaks to help with correctness and text extraction
    Signed-off-by: Taylor Price <tayworm@gmail.com>

--- pdf-reader.py
+++ pdf-reader.py
@@ -4,6 +4,7 @@ import fitz
 pdf = os.environ["FILENAME"]
 doc = fitz.open(pdf)
 text = ""
+
 for page in doc:
-    text+=page.get_text()
+    text+=page.get_text(flags=fitz.TEXTFLAGS_TEXT & ~fitz.TEXT_PRESERVE_LIGATURES & ~fitz.TEXT_PRESERVE_IMAGES)
 print(text)
